9250f656960ebc05806659efc8b0303c3080e5ab
Michael Yin <me@micy.in>
2025-11-25 16:48:07 +1300

    fix build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/env bash
+#!/bin/bash
 set -e
 
 (
     cd opencv_source
+    rm -rf ../build
     python3 platforms/apple/build_xcframework.py --out ../build \
         --iphoneos_archs arm64 \
         --iphoneos_deployment_target 14.0 \
@@ -14,3 +15,18 @@ set -e
         --build_only_specified_archs \
         --without objc
 )
+
+(cd build &&
+    tar -czvf opencv2.xcframework.tar.gz opencv2.xcframework)
+(cd build/catalyst &&
+    tar -czvf ../opencv2.framework.catalyst.tar.gz opencv2.framework)
+(cd build/iphoneos &&
+    tar -czvf ../opencv2.framework.iphoneos.tar.gz opencv2.framework)
+(cd build/iphonesimulator &&
+    tar -czvf ../opencv2.framework.iphonesimulator.tar.gz opencv2.framework)
+(cd build/macos &&
+    tar -czvf ../opencv2.framework.macos.tar.gz opencv2.framework)
+(cd build/visionos &&
+    tar -czvf ../opencv2.framework.visionos.tar.gz opencv2.framework)
+(cd build/visionsimulator &&
+    tar -czvf ../opencv2.framework.visionsimulator.tar.gz opencv2.framework)
